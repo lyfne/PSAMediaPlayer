6c38ffd0e6fe9e2760b52e1c6cce17286ef10a56
lyfne <lyf1059756966@yahoo.cn>
2013-10-08 13:05:28 +0800

    Fan:添加配置文件

---
+++ PSAMediaPlayer/PSAMediaPlayer/PSAConstantConfig.h
@@ -0,0 +1,50 @@
+//
+//  PSAConstantConfig.h
+//  PDemoS1
+//
+//  Created by 吴 wuziqi on 13-7-18.
+//  Copyright (c) 2013年 CDI. All rights reserved.
+//
+
+#ifndef PDemoS1_PSAConstantConfig_h
+#define PDemoS1_PSAConstantConfig_h
+
+// PSAMainPanelView
+#define kPSAMainPanelViewNormalWidth 916
+#define kPSAMainPanelViewMinimalWidth 808
+#define kPSAMainPanelViewHeight 614
+
+//PSAMusicViewController
+#define kPSAMusicPanelViewWeight 916
+#define kPSAMusicPanelViewHeight 460
+#define kPSAMusicPanelViewY 47
+
+// PSAAppListViewHeader
+#define kPSAStoreListHeaderViewHeight 38
+
+// Font
+#define kAvenirNextRegular @"AvenirNext-Regular"
+#define kAvenirNextDemiBold @"AvenirNext-DemiBold"
+#define kAvenirNextUltraLight @"AvenirNext-UltraLight"
+#define kAvenirNextMedium @"AvenirNext-Medium"
+
+#define kBackToHome @"BackToHome"
+
+#define kAddOptionalModelView @"AddOptionalModelView"
+#define kRemoveOptionalModelView @"RemoveOptionalModelView"
+#define kBackToMainScreen @"BackToMainScreen"
+
+// NotificationUserInfoKey
+#define kOptionalModelViewUserInfo @"kOptionalModelViewUserInfo"
+#define kOptionalModelViewSwitchTuneUserInfo @"OptionalModelViewSwitchTuneUserInfo"
+#define kFMFViewSwitchTuneUserInfo @"FMFViewSwitchTuneUserInfo"
+#define kNavigationInfoUserInfo @"kNavigationInfoUserInfo"
+
+#define kPSAAnimationDefaultDuration 0.7
+#define kPSAAnimationDefaultEaseCurve CubicEaseOut
+
+// Share
+#define kShareNotifcation @"kShareNotifcation"
+#define kShareInfoKey @"kShareInfoKey"
+
+#endif
